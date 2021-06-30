3fe276e633dc66011076227436a4ec13e2259281
Mayke Freitas <mayke.sargento@gmail.com>
2021-06-30 18:15:35 -0300

    'adding readme'

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Svelte Preprocess SCSS Bug
+
+```bash
+yarn install
+yarn dev
+```
+
+Access: http://localhost:10001
+
+Please look at `ShouldInject.svelte`, `ShouldNotInject.svelte` and `rollup.config.js`.
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,3 @@
-// this file will not afect the sandbox but will
-// afect the deployment and dowload
-
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
@@ -39,18 +36,9 @@ export default {
       ]
     }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
-    // consult the documentation for details:
-    // https://github.com/rollup/rollup-plugin-commonjs
     resolve(),
     commonjs(),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
     production && terser(),
-
     !production && serve('public')
   ].filter(Boolean)
 };
